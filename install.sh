9af4f82841e57b64cc34d67027a722c6c07e2e6e
facila <51124100+facila@users.noreply.github.com>
2020-11-21 16:21:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# version 3.00
+# version 3.01
 # l'installation se fait en root
 # se positionner dans le répertoire contenant install.sh et le fichier à installer
 # passer la commande : ./install.sh FICHIER
@@ -11,7 +11,6 @@ FILE=$1
 [ "$FILE" = '' ] && { echo "vous devez préciser l'application à installer" ; exit ; }
 [ ! -f $FILE   ] && { echo "le fichier $FILE n'existe pas" ; exit ; }
 
-DIR=/usr/local/facila
 APPLI=`echo $FILE | cut -f1 -d'.'`
 
 echo vérification des dépendances de $APPLI
@@ -26,7 +25,4 @@ esac
 [ "$ERROR" != '' ] && { echo "vous devez d'abbord installer : $ERROR" ; exit ; }
 
 echo installation de facila $FILE
-cp $FILE $DIR
-cd $DIR
-tar -xzf $FILE
-rm $FILE
+tar -xzf $FILE -C /
