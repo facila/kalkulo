8b88b794fb74f869254c71f8c910fca062cd47fb
facila <51124100+facila@users.noreply.github.com>
2020-11-22 10:56:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# version 2.02
 # l'installation se fait en root
 # se positionner dans le répertoire contenant install.sh
 # passer la commande : sh install.sh
 
-[ "`whoami`" != 'root' ] && { echo vous devez être root pour exécuter install.sh ; exit ; }
+VERSION=2.02
 
-FILE=kalkulo.2.02.tar.gz
+[ "`whoami`" != 'root' ] && { echo vous devez être root pour exécuter install.sh ; exit ; }
 
 # vérification des dépendances
 ERROR=''
-[ "`perl -v`"                = '' ] && ERROR=$ERROR"perl   "
-[ "`perl -e 'use Tk' 2>&1`" != '' ] && ERROR=$ERROR"perl-tk   "
+[ "`perl -v`"                       = '' ] && ERROR=$ERROR"perl   "
+[ "`perl -e 'use Tk' 2>/dev/null`" != '' ] && ERROR=$ERROR"perl-tk   "
 [ "$ERROR" != '' ] && { echo "vous devez d'abbord installer : $ERROR" ; exit ; }
 
+FILE=kalkulo.$VERSION.tar.gz
 echo installation de facila $FILE
 tar -xzf $FILE -C /
