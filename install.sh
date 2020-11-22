232c1509a2f62b274198800b1abb1ff2a8bf40e8
facila <51124100+facila@users.noreply.github.com>
2020-11-22 17:21:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,14 @@
 APPLI=kalkulo
 VERSION=2.02
 
-[ "`whoami`" != 'root' ] && { echo vous devez être root pour exécuter install.sh ; exit ; }
+[ "`whoami`" != 'root' ] && { echo vous devez exécuter : sudo sh install.sh ; exit ; }
 
 # vérification des dépendances
+perl -e '' 2>/dev/null ; [ $? != "0" ] && { echo "vous devez d'abbord installer : perl" ; exit ; }
+
 ERROR=''
-[ "`perl -v                 2>&1`" = '' ] && ERROR=$ERROR"perl   "
-[ "`perl -e 'use Tk'        2>&1`" = '' ] && ERROR=$ERROR"perl-tk   "
-[ "`perl -e 'use Net::Kalk' 2>&1`" = '' ] && ERROR=$ERROR"facila/Net-Kalk"
+perl -e 'use Tk'        2>/dev/null ; [ $? != "0" ] && ERROR=$ERROR"perl-tk   "
+perl -e 'use Net::Kalk' 2>/dev/null ; [ $? != "0" ] && ERROR=$ERROR"facila/Net-Kalk"
 [ "$ERROR" != '' ] && { echo "vous devez d'abbord installer : $ERROR" ; exit ; }
 
 FILE=$APPLI.$VERSION.tar.gz
