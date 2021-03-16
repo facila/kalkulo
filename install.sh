0246adf5a6fe0b05a7cfe33c7b9d15b0bd552a97
facila <facila@gmx.fr>
2021-03-16 16:45:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 # se positionner dans le répertoire contenant install.sh et le fichier tar.gz
 # exécuter la commande : sudo sh install.sh
 
+APPLI=kalkulo
 VERSION=2.11
+
 DIR=/usr/local/facila
 LG=fr_FR.UTF-8
 
@@ -17,9 +19,10 @@ perl -e 'use Tk'        2>/dev/null ; [ $? != "0" ] && ERROR=$ERROR"perl-tk   "
 perl -e 'use Net::Kalk' 2>/dev/null ; [ $? != "0" ] && ERROR=$ERROR"facila/Net-Kalk"
 [ "$ERROR" != '' ] && { echo "vous devez d'abbord installer : $ERROR" ; exit ; }
 
-FILE=kalkulo.$VERSION.tar.gz
+FILE=$APPLI.$VERSION.tar.gz
 echo installation de facila $FILE
 tar -xzf $FILE -C /
 
-# copie du répertoire LANG dans la langue de votre machine
+# copie du répertoire d'origine $LG dans la langue de votre machine $LANG
+# vous pouvez-ensuite traduire les fichiers ( menu , aide , ... )
 [ "$LANG" != "$LG" ] && cp -R $DIR/var/$LG $DIR/var/$LANG
